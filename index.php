c25a0a00314765236464285b98df8705d229e54c
moedeera <82620264+moedeera@users.noreply.github.com>
2024-05-03 08:21:44 -0600

    committ

--- index.php
+++ index.php
@@ -1,10 +1,45 @@
 <?php 
 
+$_ENV = parseEnv();
+
+function path($relativePath): string
+{
+    return __DIR__ . "/" . $relativePath;
+}
+
+
+function parseEnv(): array
+{
+    $lines = file(path(".env"), FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    $config = [];
+    foreach ($lines as $line) {
+        // Ignore comments
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($key, $value) = explode('=', $line, 2) + [NULL, NULL];
+        if ($key !== NULL && $value !== NULL) {
+            $config[trim($key)] = trim($value);
+        }
+    }
+
+    return $config;
+}
+
+
+
 $uri = $_SERVER['REQUEST_URI'];
 
-$root = "";
+$root = $_ENV["ROOT"] ?? "";
+
+ 
+// echo $root ;
+// die() ;
 
 switch ($uri) {
+  
     case $root . "/":
         require "views/home/index.php";
         break;
@@ -12,7 +47,9 @@ switch ($uri) {
         require "views/about/index.php";
         break;
      default:
+     echo $root . $uri;
          require "error.php"   ;
          break;
+
 };
 
