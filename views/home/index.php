79c6231361b340110c65aab673169e6a36120834
moedeera <82620264+moedeera@users.noreply.github.com>
2024-05-03 08:02:51 -0600

    committ

---
+++ views/home/index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>My PHP Website</title>
+</head>
+<body >
+  <!-- You can output PHP including variables, etc -->
+  <h1>Hello </h1>
+  <!-- You may only drop the semi-colon after a statement when the statement is followed immediately by a closing PHP tag ?>. -->
+  <h1>Hello heroku from PHP ??</h1>
+  <a href="/about">About page</a>
+</body>
+</html>
