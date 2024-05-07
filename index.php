80ee5f66479e799fdab3d6a5b2b651dfbf8cb842
Moe Deera <deeramoe@gmail.com>
2024-05-07 02:05:36 -0600

    committ

--- index.php
+++ index.php
@@ -1,66 +1,21 @@
 <?php 
 
-
-// helper functions
-require './helpers.php';
-
-// Server Initiation //
-$_ENV = parseEnv();
-
-function path($relativePath): string
-{
-    return __DIR__ . "/" . $relativePath;
-}
-
-
-function parseEnv(): array
-{
-    $lines = file(path(".env"), FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-    $config = [];
-    foreach ($lines as $line) {
-        // Ignore comments
-        if (strpos(trim($line), '#') === 0) {
-            continue;
-        }
-
-        list($key, $value) = explode('=', $line, 2) + [NULL, NULL];
-        if ($key !== NULL && $value !== NULL) {
-            $config[trim($key)] = trim($value);
-        }
-    }
-
-    return $config;
-}
-
-
-
-$uri = $_SERVER['REQUEST_URI'];
-
-$root = $_ENV["ROOT"] ?? "";
-
- 
-function addRoute($route, $data){
-echo $route . $data;
-}
-
-// echo $root ;
-// die() ;
-
-switch ($uri) {  
-    case $root . "/":
-        require "views/home/index.php";
-        break;
-    case $root . "/about":
-        require "views/about/index.php";
-        break;
-   case $root . "/posts/:id": 
-       addRoute("/example", "YurController.php") ;
-        break;   
-     default:
-     echo $root . $uri;
-         require "error.php"   ;
-         break;
-
-};
-
+echo "hello";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>My PHP Website</title>
+</head>
+<body >
+  <!-- You can output PHP including variables, etc -->
+  <h1>Hello </h1>
+  <!-- You may only drop the semi-colon after a statement when the statement is followed immediately by a closing PHP tag ?>. -->
+  <h1>Hello heroku from PHP ??</h1>
+  <a href="/about">About page</a>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,66 @@
+<?php 
+
+
+// helper functions
+require './helpers.php';
+
+// Server Initiation //
+$_ENV = parseEnv();
+
+function path($relativePath): string
+{
+    return __DIR__ . "/" . $relativePath;
+}
+
+
+function parseEnv(): array
+{
+    $lines = file(path(".env"), FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    $config = [];
+    foreach ($lines as $line) {
+        // Ignore comments
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($key, $value) = explode('=', $line, 2) + [NULL, NULL];
+        if ($key !== NULL && $value !== NULL) {
+            $config[trim($key)] = trim($value);
+        }
+    }
+
+    return $config;
+}
+
+
+
+$uri = $_SERVER['REQUEST_URI'];
+
+$root = $_ENV["ROOT"] ?? "";
+
+ 
+function addRoute($route, $data){
+echo $route . $data;
+}
+
+// echo $root ;
+// die() ;
+
+switch ($uri) {  
+    case $root . "/":
+        require "views/home/index.php";
+        break;
+    case $root . "/about":
+        require "views/about/index.php";
+        break;
+   case $root . "/posts/:id": 
+       addRoute("/example", "YurController.php") ;
+        break;   
+     default:
+     echo $root . $uri;
+         require "error.php"   ;
+         break;
+
+};
+
