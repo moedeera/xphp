39a7fd1f36b756f5fac279230b2cc636868efafc
moedeera <82620264+moedeera@users.noreply.github.com>
2024-05-10 04:46:51 -0600

    commit

--- views/about/index.php
+++ views/about/index.php
@@ -1,1 +1,18 @@
-About
+<?php
+
+include basePath('./views/partials/navbar.php')
+?>
+<?php
+
+require basePath('./views/partials/head.php')
+?>
+
+
+<body >
+  <!-- You can output PHP including variables, etc -->
+  <h1>About </h1>
+  <!-- You may only drop the semi-colon after a statement when the statement is followed immediately by a closing PHP tag ?>. -->
+  <h1>About Page</h1>
+  <a href="./about">About page</a>
+</body>
+</html>
