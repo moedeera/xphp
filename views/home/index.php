c260cae2dde2086c823b362012fcc6d39058bdfc
moedeera <82620264+moedeera@users.noreply.github.com>
2024-06-05 21:22:45 -0600

    committ

--- views/home/index.php
+++ views/home/index.php
@@ -1,8 +1,7 @@
-
-
+<?php 
+basePath('./views/partials/navbar.php'); ?> 
 <?php
- basePath('./views/partials/navbar.php')
-?>
+ basePath('./views/partials/head.php'); ?>
 
 <?php 
 
@@ -12,31 +11,6 @@ $stmt-> execute();
 //fetch results
 $results = $stmt->fetchAll();
 
-
-$data1 = [
-  ['name' => "John Smith", 'age' => 20, 'graduated' => false],
-  ['name' => "Samantha Jones", 'age' => 22, 'graduated' => true],
-  ['name' => "Cindy Walker", 'age' => 25, 'graduated' => false],
-  ['name' => "Winston Hughes", 'age' => 27, 'graduated' => true]
-];
-
-$data2 = [
-  ['name' => "John Smith", 'age' => 20, 'graduated' => false],
-  ['name' => "Samantha Jones", 'age' => 22, 'graduated' => true],
-  ['name' => "Cindy Walker", 'age' => 25, 'graduated' => false],
-  ['name' => "Winston Hughes", 'age' => 27, 'graduated' => true]
-];
-
-$variable1= "Student data";
-$format = $_GET['format'] ?? 'html'; // Check for a 'format' parameter in URL
-$dataset = $_GET['dataset'] ?? 'data1';
-$response = ($dataset === 'data2') ? $data2 : $data1;
-
-if ($format === 'json') {
-  header('Content-Type: application/json');
-  echo json_encode($response);
-  exit; // Stop script execution after JSON output
-}
 
 ?>
 
@@ -49,15 +23,6 @@ if ($format === 'json') {
 
 
 
-<div class="mini-container">
-  <?php  include './views/partials/card.php';
-foreach ($data1 as $key=>$value):
-$student = $value;
-echo generateStudentHtml($student);
-endforeach; ?>
-
-<button class="btn">Update</button>
- </div>
 
 
 </div>
