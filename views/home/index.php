c2bfc8e1b6a922e7f0c795a038b8806d84b7908f
moedeera <82620264+moedeera@users.noreply.github.com>
2024-06-06 17:54:08 -0600

    committ

--- views/home/index.php
+++ views/home/index.php
@@ -6,17 +6,14 @@ basePath('./views/partials/navbar.php'); ?>
 <?php 
 
 
-$stmt = $pdo->prepare('SELECT * FROM employee');
-$stmt-> execute();
-//fetch results
-$results = $stmt->fetchAll();
+
 
 
 ?>
 
 
 <div class="container" >
-  <!-- You can output PHP including variables, etc -->
+ <!-- You can output PHP including variables, etc -->
   <?php  include './views/partials/block1.php';
   echo generateBlock1();
   ;?>
