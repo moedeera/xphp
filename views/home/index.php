ab4c4725fa97cc81066a8154c68d02e50a1710f1
Moe Deera <deeramoe@gmail.com>
2024-06-22 06:18:10 -0600

    committ

--- views/home/index.php
+++ views/home/index.php
@@ -13,7 +13,22 @@ basePath('./views/partials/navbar.php'); ?>
 
 
 <div class="container" >
-  <div class="container-bg-image"></div>
+  <div class="container-bg-image"> <video autoplay muted loop id="background-video">
+      <source src="https://videos.pexels.com/video-files/3998508/3998508-uhd_2732_1440_25fps.mp4" type="video/mp4">
+      Your browser does not support HTML5 video.
+    </video></div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+      const video = document.getElementById('background-video');
+      const thumbnail = document.getElementById('background-thumbnail');
+
+      video.oncanplaythrough = function () {
+        video.classList.add('loaded');
+        thumbnail.classList.add('hidden');
+        video.play();
+      };
+    });
+    </script>
   <div class="container-bg-overlay-color"></div>
  <!-- You can output PHP including variables, etc -->
 <?php include './views/partials/html/landing.html';?>
